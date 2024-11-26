277ce0aeae9b70434326a5d3d5ba8a229f7855be
AbdelrahmanAkll <abdelrahman.akl@espace.com.eg>
2024-11-26 20:58:22 +0200

    first commit

---
+++ src/test2/Solution.java
@@ -0,0 +1,12 @@
+package test2;
+
+
+
+public class Solution  {
+
+
+
+}
+
+
+
---
+++ src/test2/Testtt.java
@@ -0,0 +1,19 @@
+package test2;
+
+
+import java.util.Scanner;
+
+public class Testtt {
+
+    public static void main(String [] args){
+
+    }
+
+
+
+
+
+}
+
+
+
